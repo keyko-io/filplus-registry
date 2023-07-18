a3e76001082441216359abe359dd201cdfc9ce6c
hcsoylu <hcsoylu.dev@gmail.com>
2023-07-18 13:38:29 +0300

    added 2 view tabs grid-table

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,39 +4,56 @@ import { Input } from '@/components/ui/input'
 import {
   Select,
   SelectContent,
-  SelectGroup,
   SelectItem,
-  SelectLabel,
   SelectTrigger,
   SelectValue,
 } from '@/components/ui/select'
+import { Tabs, TabsContent, TabsList, TabsTrigger } from '@/components/ui/tabs'
 import mockData from '@/mockData.json'
+import { Search } from 'lucide-react'
 
 export default function Home() {
   return (
-    <main className="mt-10 px-10 grid gap-6">
-      <div className="flex space-x-4">
-        <Input
-          type="search"
-          placeholder="Search..."
-          className="md:w-[100px] lg:w-[300px]"
-          onChange={(e) => console.log(e.target.value)}
-        />
-        <Select onValueChange={(value) => console.log(value)}>
-          <SelectTrigger id="area" className="w-[180px]">
-            <SelectValue placeholder="Filter Applications" />
-          </SelectTrigger>
-          <SelectContent>
-            <SelectItem value="all">All</SelectItem>
-            <SelectItem value="efil">Efil+</SelectItem>
-          </SelectContent>
-        </Select>
-      </div>
-      <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 ">
-        {mockData.map((app) => (
-          <AppCard application={app} key={app.projectId} />
-        ))}
-      </div>
+    <main className="mt-10 px-10 grid">
+      <Tabs defaultValue="grid">
+        <div className="flex items-center justify-between mb-8">
+          <div className="flex items-center space-x-4">
+            <div className="flex items-center relative">
+              <Search className="absolute left-2" size={20} />
+              <Input
+                type="search"
+                placeholder="Search Application..."
+                className="md:w-[100px] lg:w-[300px] pl-10"
+                onChange={(e) => console.log(e.target.value)}
+              />
+            </div>
+
+            <Select onValueChange={(value) => console.log(value)}>
+              <SelectTrigger id="area" className="w-[180px]">
+                <SelectValue placeholder="Filter Applications" />
+              </SelectTrigger>
+              <SelectContent>
+                <SelectItem value="all">All</SelectItem>
+                <SelectItem value="efil">Efil+</SelectItem>
+              </SelectContent>
+            </Select>
+          </div>
+
+          <TabsList>
+            <TabsTrigger value="grid">Grid View</TabsTrigger>
+            <TabsTrigger value="table">Table View</TabsTrigger>
+          </TabsList>
+        </div>
+
+        <TabsContent value="grid">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-6 ">
+            {mockData.map((app) => (
+              <AppCard application={app} key={app.projectId} />
+            ))}
+          </div>
+        </TabsContent>
+        <TabsContent value="table">Table View</TabsContent>
+      </Tabs>
     </main>
   )
 }
--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,10 +1,10 @@
 'use client'
-import { signIn, signOut, useSession } from 'next-auth/react'
+import { signIn, useSession } from 'next-auth/react'
 import Link from 'next/link'
 import React from 'react'
 import { Button } from './ui/button'
 import { Github } from 'lucide-react'
-//import { Avatar, AvatarFallback, AvatarImage } from '@/components/ui/avatar'
+import UserNav from './UserNav'
 
 const Navbar = () => {
   const session = useSession()
@@ -13,15 +13,13 @@ const Navbar = () => {
     <div className="h-16 shadow-md flex items-center px-12 justify-between">
       <Link href="/">Filecoin Plus</Link>
 
-      <div className="flex space-x-4 items-center">
-        <div>About</div>
-
+      <div>
         {session.status !== 'authenticated' ? (
           <Button onClick={() => signIn('github')}>
             <Github className="mr-2 h-4 w-4" /> Login with Github
           </Button>
         ) : (
-          <Button onClick={() => signOut()}>Logout</Button>
+          <UserNav />
         )}
       </div>
     </div>
