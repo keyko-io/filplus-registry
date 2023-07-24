28fb2df1452162be6e351b4c361d1cb776acd91d
hcsoylu <hcsoylu.dev@gmail.com>
2023-07-24 18:13:46 +0300

    testing api endpoint

--- src/lib/apiClient.ts
+++ src/lib/apiClient.ts
@@ -1,5 +1,5 @@
 import axios from 'axios'
 
 export default axios.create({
-  baseURL: 'https://jsonplaceholder.typicode.com',
+  baseURL: 'https://fp-core.6omfj573u6naa.us-east-2.cs.amazonlightsail.com/',
 })
