4d423fb61e55752d8580f748333ccc03134a7d3e
hcsoylu <hcsoylu.dev@gmail.com>
2023-07-25 14:59:36 +0300

    adding tpyes

--- src/type.ts
+++ src/type.ts
@@ -1,30 +1,51 @@
-export interface App {
-  dataCapApplicationType: string[]
-  projectId: string
-  applicationInfo: ApplicationInfo
+export interface Application {
+  id: string
+  info: Info
+  _type: string
 }
 
-export interface ApplicationInfo {
-  'Core Information': CoreInformation
-  dataCapAllocations: DataCapAllocation[]
+export interface Info {
+  core_information: CoreInformation
+  application_lifecycle: ApplicationLifecycle
+  datacap_allocations: DatacapAllocation[]
+}
+
+export interface ApplicationLifecycle {
+  first_allocation_time: string
+  initial_pr_number: number
+  is_active: boolean
+  state: string
+  time_of_new_state: string
+  validated_by: string
+  validated_time: string
 }
 
 export interface CoreInformation {
-  'Data Owner Name': string
-  'Data Owner Country/Region': string
-  'Data Owner Industry': string
-  Website: string
-  'Social Media': string[]
+  data_owner_industry: string
+  data_owner_name: string
+  data_owner_region: string
+  social_media: string
+  website: string
 }
 
-export interface DataCapAllocation {
+export interface DatacapAllocation {
+  request_information: RequestInformation
+  signers: Signer[]
+}
+
+export interface RequestInformation {
+  actor: string
+  allocation_amount: string
+  client_address: string
+  created_at: string
+  is_active: boolean
+  request_id: string
+  request_type: string
   uuid: string
-  clientAddress: string
-  timeOfRequest: number
-  timeOfAllocation: number
-  multisig: string
-  allocationAmount: string
-  signers: any[]
-  pr: number
-  'pr-cid': string
+}
+
+export interface Signer {
+  message_cid: string
+  signing_address: string
+  time_of_signature: string
 }
